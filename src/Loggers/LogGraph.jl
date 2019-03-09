ea6a84a67ee6da9403d660258878a2ff27c53c78
shashikdm <shashikant.123kadam@gmail.com>
2019-03-09 19:44:36 +0530

    added support for multiple layers of same type/name by appending an integer to it eg Dense, Dense_1, Dense_2 etc

--- src/Loggers/LogGraph.jl
+++ src/Loggers/LogGraph.jl
@@ -5,18 +5,7 @@
 
 using Flux
 
-function log_graph(logger::Logger, name::String, value::Tuple)
-    @info "WIP"
-    #summ    = SummaryCollection()
-    #push!(summ.value, graph_summary(name, value))
-    graph_def = graph_summary(name, value)
-    iob = PipeBuffer()
-    writeproto(iob, graph_def)
-    serial_graph_def = iob.data
-    write_event(logger.file, Event(graph_def = serial_graph_def))
-end
 function log_graph(logger::Logger, name::String, value::Chain)
-    @info "WIP"
     #summ    = SummaryCollection()
     #push!(summ.value, graph_summary(name, value))
     graph_def = graph_summary(name, value)
@@ -28,10 +17,19 @@ end
 function graph_summary(name::String, model::Chain)
 	@info "WIP"
 	function unravel(expr::Expr)
-		@info expr
+		#@info expr
 		dead_end = true #means cannot expand further
 		if expr.head == :call
-			push!(prefixes, string(expr.args[1]))
+			node_name = string(expr.args[1])
+			incr = 1
+			for name in Names #this is to allow repeated nodes of same type by appending a number
+				if name == node_name
+					node_name = node_name*"_$incr"
+					incr += 1
+				end
+			end
+			push!(prefixes, node_name)
+			push!(Names, node_name)
 			for arg in expr.args[2:end]
 				if typeof(arg) == Expr
 					dead_end = false #can be further expanded
@@ -41,12 +39,21 @@ function graph_summary(name::String, model::Chain)
 			pop!(prefixes)
 		end
 		if dead_end == true
-			name = join(prefixes,"/")*"/"*string(expr)
-			push!(Nodes, NodeDef(name = name, op = name, input = Vector([prevnode]))) #insert inner nodes
-			prevnode = name
+		    node_name = join(prefixes,"/")*"/"*string(expr)
+			incr = 1
+			for name in Names #this is to allow repeated nodes of same type by appending a number
+				if name == node_name
+					node_name = node_name*"_$incr"
+					incr += 1
+				end
+			end
+			push!(Names, node_name)
+			push!(Nodes, NodeDef(name = node_name, op = node_name, input = Vector([prevnode]))) #insert inner nodes
+			prevnode = node_name
 		end
 	end
 	Nodes = []
+	Names = []
 	prevnode = "Init"
 	push!(Nodes, NodeDef(name = prevnode, op = prevnode)) #insert init node
 	prefixes = []
@@ -55,31 +62,12 @@ function graph_summary(name::String, model::Chain)
 	push!(Nodes, NodeDef(name = "Out", op = "Out", input = Vector([prevnode]))) #insert Out node
 	GraphDef(node = Vector(Nodes))
 end
-function graph_summary(name::String, model::Tuple) #old method
-	@info "WIP"
-	"""
-	GraphDef takes 4 arguments
-	node : vector of type NodeDef
-	versions : of type VersionDef (Producer, minconsumer, badconsumer)
-	version : of type Int32 deprecated
-	library : of type FunctionDefLibrary (experimental / remove)
-	"""
-	Node = []
-	prevnode = "Init"
-	push!(Node, NodeDef(name = prevnode, op = prevnode)) #insert init node
-	for layer in model #Iterate over each layer
-		name = "Chain/"*repr(typeof(layer).name) #Obtain name of the layer (The Datatype: Dense, Conv etc)
-		push!(Node, NodeDef(name = name, op = name, input = Vector([prevnode])))
-		prevnode = name
-	end
-	GraphDef(node = Vector(Node))
-end
 
 """
 import TensorBoardLogger
 using .TensorBoardLogger
 using Flux
-model = Chain(Dense(20, 20, relu), x -> 2*x, softmax)
+model = Chain(Dense(20, 20, relu), Dense(20, 20, leakyrelu), Dense(20, 20), Dense(20, 20), x -> 2*x, softmax)
 lgr = Logger("/home/shashi/TensorBoardLogger")
 log_graph(lgr, "string", model)
 """
