8dc8b9aabd4290501f4c7d6dfe1b66fa7c1dc6ee
Filippo Vicentini <filippovicentini@gmail.com>
2019-04-24 12:03:28 +0200

    Add some tests for scalar and histogram summary creation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using TensorBoardLogger, Logging
+using TensorBoardLogger: preprocess, summary_impl
 using Test
 
 @testset "Initialization" begin
@@ -19,16 +20,35 @@ using Test
 end
 
 @testset "Scalar Value Logger" begin
-    logger = TBLogger("log/")
+    logger = TBLogger("test_logs/t", tb_overwrite)
     step = 1
+
+    ss = TensorBoardLogger.scalar_summary("test", 12.0)
+    @test isa(ss, TensorBoardLogger.Summary_Value)
+    @test ss.simple_value == 12.0
+    @test ss.tag == "test"
+
     log_value(logger, "float32", 1.25f0, step=step)
     log_value(logger, "float64", 1.5, step=step)
     log_value(logger, "irr", pi, step=step)
     log_value(logger, "complex", 1.0 + 1.0im, step=step)
 end
 
+@testset "Scalar Value processing interface" begin
+    data = Vector{Pair{String,Any}}()
+    @test data == preprocess("test1", 1.0, data)
+    @test first(data[1])=="test1"
+    @test last(data[1])==1.0
+
+    data == preprocess("test2", 1.0+0.5im, data)
+    @test first(data[2])=="test2/re"
+    @test last(data[2])==1.0
+    @test first(data[3])=="test2/im"
+    @test last(data[3])==0.5
+end
+
 @testset "Histogram Value Logger" begin
-    logger = TBLogger("log/")
+    logger = TBLogger("test_logs/t", tb_overwrite)
     step = 1
 
     x0 = 0.5+step/30; s0 = 0.5/(step/20);
@@ -37,9 +57,20 @@ end
     histvals = [exp(-((c-x0)/s0)^2) for c=centers]
     data_tuple = (edges, histvals)
 
+    ss = TensorBoardLogger.histogram_summary("test", edges, histvals)
+    @test isa(ss, TensorBoardLogger.Summary_Value)
+    @test ss.tag == "test"
+    @test isa(ss.histo, TensorBoardLogger.HistogramProto)
+    @test ss.histo.min == minimum(edges)
+    @test ss.histo.max == maximum(edges)
+    @test all(ss.histo.bucket_limit.== edges[2:end])
+    @test all(ss.histo.bucket.== histvals)
+
     log_histogram(logger, "hist/cust", data_tuple, step=step)
     log_histogram(logger, "hist/cust", rand(100), step=step)
+    log_histogram(logger, "hist/cust", rand(10,10), step=step)
 end
+
 
 @testset "LogInterface" begin
     logger = TBLogger("log/")
