8282a13cad6f121f977a65d20da1f3d339c8ed2c
shashikdm <shashikant.123kadam@gmail.com>
2019-05-16 21:01:57 +0530

    add TBImage, TBImages wrapper

--- src/logger_dispatch_overrides.jl
+++ src/logger_dispatch_overrides.jl
@@ -24,6 +24,156 @@ preprocess(name, val::WrapperLogType, data) = push!(data, name=>val)
 Base.show(io::IO, mime::AbstractString, x::WrapperLogType) =
     Base.show(io, mime, content(x))
 
+########## For things going to LogImage ########################
+"""
+    TBImage(data)
+
+Forces `data` to be serialized as an histogram to TensorBoard.
+"""
+struct TBImages <:WrapperLogType
+    data::AbstractArray
+    format::ImageFormat
+content(x::TBImages) = x.data
+function preprocess(name, val::TBImages, data)
+    n = 1
+    for img in val.data
+        preprocess(name*"/$n", TBImage(img, val.format), data)
+    end
+end
+
+struct TBImage <: WrapperLogType
+    data::AbstractArray
+    format::ImageFormat
+end
+content(x::TBImage) = x.data
+function preprocess(name, val::TBImage, data)
+    imgArray = val.data
+    formatdict = Dict(
+    L => function(imgArray)
+        @assert ndims(imgArray) == 1
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    CL => function(imgArray)
+        @assert ndims(imgArray) == 2
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    LC => function(imgArray)
+        @assert ndims(imgArray) == 2
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    HW => function(imgArray)
+        @assert ndims(imgArray) == 2
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    WH => function(imgArray)
+        @assert ndims(imgArray) == 2
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    HWC => function(imgArray)
+        @assert ndims(imgArray) == 3
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    WHC => function(imgArray)
+        @assert ndims(imgArray) == 3
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    CHW => function(imgArray)
+        @assert ndims(imgArray) == 3
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    CWH => function(imgArray)
+        @assert ndims(imgArray) == 3
+        push!(data, name=>TBImage(imgArray, val.format))
+    end,
+    HWN => function(imgArray)
+        @assert ndims(imgArray) == 3
+        N = size(imgArray, 3)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[:, :, n], HW))
+        end
+    end,
+    WHN => function(imgArray)
+        @assert ndims(imgArray) == 3
+        N = size(imgArray, 3)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[:, :, n], WH))
+        end
+    end,
+    NHW => function(imgArray)
+        @assert ndims(imgArray) == 3
+        N = size(imgArray, 1)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[n, :, :], HW))
+        end
+    end,
+    NWH => function(imgArray)
+        @assert ndims(imgArray) == 3
+        N = size(imgArray, 1)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[n, :, :], WH))
+        end
+    end,
+    HWCN => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 4)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[:, :, :, n], HWC))
+        end
+    end,
+    WHCN => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 4)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[:, :, :, n], WHC))
+        end
+    end,
+    CHWN => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 4)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[:, :, :, n], CHW))
+        end
+    end,
+    CWHN => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 4)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[:, :, :, n], CWH))
+        end
+    end,
+    NHWC => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 1)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[n, :, :, :], HWC))
+        end
+    end,
+    NWHC => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 1)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[n, :, :, :], WHC))
+        end
+    end,
+    NCHW => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 1)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[n, :, :, :], CHW))
+        end
+    end,
+    NCWH => function(imgArray)
+        @assert ndims(imgArray) == 4
+        N = size(imgArray, 1)
+        for n in 1:N
+            push!(data, name*"/n"=>TBImage(imgArray[n, :, :, :], CWH))
+        end
+    end
+    )
+    formatdict[val.format](val.data)
+end
+summary_impl(name, val::TBImage) = image_summary(name, val.data, val.format)
+
 ########## For things going to LogText ##############################
 """
     TBText(data)
