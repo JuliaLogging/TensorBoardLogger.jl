a96a0947dd080eb93418cb0044da341fe6b7b733
shashikdm <shashikant.123kadam@gmail.com>
2019-05-28 16:37:37 +0530

    add log_graph

--- src/Loggers/LogGraph.jl
+++ src/Loggers/LogGraph.jl
@@ -1,6 +1,8 @@
 """
-    log_graph(logger, name, model)
-	currenly logs a Chain variable with name
+    log_graph(logger::TBLogger, name::AbstractString, model::Chain, dummy)
+logs a Chain variable with name `name`
+- value: A model of datatype Chain
+- dummy: dummy input data to the model
 """
 
 function log_graph(logger::TBLogger, name::AbstractString, value::Chain, dummy; step = nothing)
@@ -10,23 +12,45 @@ end
 
 function graph_summary(name::String, model::Chain, dummy)
 	@info "WIP"
-	namepool = Dict{String, Integer}() #required to give unique name to each node
-	prefixes = Vector{String}() #to make subgraphs
-	Nodes = Vector{NodeDef}() #all nodes to be plotted
-	Tarrays = Dict{Any, NodeDef}() #keeping track of tracked arrays :P
-	Input = dummy
-	#Create input node using dummy
-	init_node_name = "Input"
-	init_node_op = "input"
-	init_node_attr = Dict("_output_shapes" => AttrValue(list = AttrValue_ListValue(shape = [TensorShapeProto(dim = [TensorShapeProto_Dim(size = x) for x in (collect(size(Input)))])])),
-	"shape" => AttrValue(list = AttrValue_ListValue(shape = Vector([TensorShapeProto(dim = Vector([TensorShapeProto_Dim(size = x) for x in (collect(size(Input)))]))]))),
-	"dtype" => AttrValue(s = Array{UInt8}(repr(typeof(Input)))))
-	init_node = NodeDef(name = init_node_name, op = init_node_op, attr = init_node_attr)
-	push!(Nodes, init_node)
-	namepool[init_node_name] = 0
-	Tarrays[Input] = init_node
-	function operation(Tnode)
+	#utility functions
+	#function to get node attributes
+	function getnodeattr(tarray)
+		#determine type of tarray
+		dtype = eltype(tarray.data)
+		nodetype =
+		dtype == nothing			? _DataType.DT_INVALID :
+		dtype == UInt8				? _DataType.DT_UINT8 :
+		dtype == UInt16				? _DataType.DT_UINT16 :
+		dtype == UInt32				? _DataType.DT_UINT32 :
+		dtype == UInt64				? _DataType.DT_UINT64 :
+		dtype == Int8				? _DataType.DT_INT8 :
+		dtype == Int16				? _DataType.DT_INT16 :
+		dtype == Int32				? _DataType.DT_INT32 :
+		dtype == Int64				? _DataType.DT_INT64 :
+		dtype == Float16			? _DataType.DT_BFLOAT16 :
+		dtype == Float32			? _DataType.DT_FLOAT :
+		dtype == Float64			? _DataType.DT_DOUBLE :
+		dtype == AbstractString		? _DataType.DT_STRING :
+		dtype == Bool				? _DataType.DT_BOOL :
+		dtype ∈ [Complex{Float32},
+				Complex{Float16},
+				Complex{UInt8},
+				Complex{UInt16},
+				Complex{UInt32},
+				Complex{Int8},
+				Complex{Int16},
+				Complex{Int32}]		? _DataType.DT_COMPLEX64 :
+		dtype ∈ [Complex{Float64},
+				Complex{UInt64},
+				Complex{Int64}]		? _DataType.DT_COMPLEX128 :
+		throw("Unknown Datatype")
+
+		nodeattr = Dict("_output_shapes" => AttrValue(list = AttrValue_ListValue(shape = [TensorShapeProto(dim = [TensorShapeProto_Dim(size = x) for x in (collect(size(tarray)))])])),
+						"shape" => AttrValue(list = AttrValue_ListValue(shape = [TensorShapeProto(dim = [TensorShapeProto_Dim(size = x) for x in (collect(size(tarray)))])])),
+						"dtype" => AttrValue(_type = nodetype))
+	end
 	#creates nodes using Tracked Arrays
+	function operation(Tnode)
 		function digdeeper(f) #dig deep into tracker to find input arrays
 			if isa(f, Module) || isa(f, DataType) || isa(f, Nothing)
 				return
@@ -40,8 +64,11 @@ function graph_summary(name::String, model::Chain, dummy)
 				end
 			end
 		end
-		#lookup if Tnode already exists
+		function getnodename(op)
+			return String(methods(op).mt.name)
+		end
 		Tinputs = []
+		#lookup if Tnode already exists
 		if haskey(Tarrays, Tnode) #what will i do if it already exists?
 			return Tarrays[Tnode].name
 		else
@@ -49,21 +76,19 @@ function graph_summary(name::String, model::Chain, dummy)
 			#First determine the node name which is the operation performed which resulted in this trackedarray
 			#keep going deep inside tracker to precisely identify the operation and arguments
 			op = Tnode.tracker.f.func
+			#Todo: identify opname
+			node_name = getnodename(op)
 			if !isa(op, Nothing)#this is a not a leaf
 				digdeeper(op)
 			end
-			node_name = "op"
+			node_op = node_name
 			if haskey(namepool, node_name)
 				namepool[node_name] += 1
 			else
 				namepool[node_name] = 0
 			end
-			node_op = lowercase(node_name)
-			node_name = join(prefixes,"/")*"/" * node_name *"_"* string(namepool[node_name])
-			node_shape = Vector([TensorShapeProto(dim = Vector([TensorShapeProto_Dim(size = x) for x in (collect(size(Tnode)))]))])
-			node_attr = Dict("_output_shapes" => AttrValue(list = AttrValue_ListValue(shape = node_shape)),
-			"shape" => AttrValue(list = AttrValue_ListValue(shape = node_shape)),
-			"dtype" => AttrValue(s = Array{UInt8}(repr(typeof(Tnode)))))
+			node_name = join(prefix,"/")*"/" * node_name *"_"* string(namepool[node_name])
+			node_attr = getnodeattr(Tnode)
 			node_input = Vector([operation(X) for X in Tinputs])
 			if isempty(Tinputs)
 				new_node = NodeDef(name = node_name, op = node_op, attr = node_attr)
@@ -75,59 +100,66 @@ function graph_summary(name::String, model::Chain, dummy)
 			return node_name
 		end
 	end
-	function applychain(model)
-	# creates prefixes for subgraphs
+	#propagates dummy through model and generates graph
+	# creates prefix for subgraphs
 	# recursively calls applychain if argument is another Chain
 	# calls trackfunction on each layer
+	function applychain(model)
 		if haskey(namepool, "Chain") #Check if Chain is already there in nodes
 			namepool["Chain"] += 1
 		else
 			namepool["Chain"] = 0
 		end
-		push!(prefixes,"Chain_"*string(namepool["Chain"]))
+		push!(prefix,"Chain_"*string(namepool["Chain"]))
 		for layer in model.layers
-			if isa(layer, Chain)
-				#new subgraph for this function
+			if isa(layer, Chain) #another subgraph inside this function
 				applychain(layer)
-			else
-				#create new subgraph for this function
+			else #create new subgraph for this function
 				layer_ast = Meta.parse(repr(layer))
-				prefix = string(layer_ast.args[1])
-				if haskey(namepool, prefix)
-					namepool[prefix] += 1
+				subgraphname = string(layer_ast.args[1]) #gets name of the function
+				#Todo: get arguments too
+
+				if haskey(namepool, subgraphname)
+					namepool[subgraphname] += 1
 				else
-					namepool[prefix] = 0
+					namepool[subgraphname] = 0
 				end
-				prefix = prefix *"_"* string(namepool[prefix])
-				push!(prefixes, prefix)
-				Output = layer(Input)
-				input = operation(Output)
+				subgraphname = subgraphname *"_"* string(namepool[subgraphname])
+				push!(prefix, subgraphname)
+				Output = layer(Input)#forward propagation through this layer
+				input = operation(Output)#perform operation to get opnodes
 				Input = Output
-				pop!(prefixes)
+				pop!(prefix)
 			end
 		end
-		pop!(prefixes)
-	end
-	if isa(model, Chain)
-		applychain(model)
-	else
-		Output = model(Input)
-		input = operation(Output)
-		Input = Output
+		pop!(prefix)
 	end
 
+	namepool = Dict{String, Integer}() #required to give unique name to each node
+	prefix = Vector{String}() #to make subgraphs
+	Nodes = Vector{NodeDef}() #all nodes to be plotted
+	Tarrays = Dict{Any, NodeDef}() #keeping track of tracked arrays :P
+	Input = dummy
+
+	#Create input node using dummy
+	init_node_name = "Input"
+	init_node_op = "input"
+	init_node_attr = getnodeattr(Input)
+	init_node = NodeDef(name = init_node_name, op = init_node_op, attr = init_node_attr)
+	push!(Nodes, init_node)
+	namepool[init_node_name] = 0
+	Tarrays[Input] = init_node
+	input = ""
+
+	applychain(model)
+
+	#Create output node
 	fin_node_name = "Output"
 	fin_node_op = "output"
-	fin_node_attr = Dict("_output_shapes" => AttrValue(list = AttrValue_ListValue(shape = Vector([TensorShapeProto(dim = Vector([TensorShapeProto_Dim(size = x) for x in (collect(size(Input)))]))]))),
-	"shape" => AttrValue(list = AttrValue_ListValue(shape = Vector([TensorShapeProto(dim = Vector([TensorShapeProto_Dim(size = x) for x in (collect(size(Input)))]))]))),
-	"dtype" => AttrValue(s = Array{UInt8}(repr(typeof(Input)))))
+	fin_node_attr = getnodeattr(Input)
 	fin_node_input = Vector([input])
 	last_node = NodeDef(name = fin_node_name, op = fin_node_op, attr = fin_node_attr, input = fin_node_input)
 	push!(Nodes, last_node)
-	@info "Nodelist:"
-	# for node in Nodes
-	# 	@info node
-	# end
 	return GraphDef(node = Vector(Nodes))
 end
 """
