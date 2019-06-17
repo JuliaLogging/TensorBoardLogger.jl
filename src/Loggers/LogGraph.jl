bbc742dde3fecda3ffdf2228933c19c7f483d587
shashikdm <shashikant.123kadam@gmail.com>
2019-06-17 11:40:46 +0530

    - change assert statements - change getdatatype -> jltype2tf - change @error -> throw

--- src/Loggers/LogGraph.jl
+++ src/Loggers/LogGraph.jl
@@ -2,44 +2,15 @@
 	log_graph
 """
 function log_graph(logger::TBLogger, g::AbstractGraph; step = nothing, nodelabel::Vector{String} = map(string, vertices(g)), nodeop::Vector{String} = map(string, vertices(g)), nodedevice::Vector{String} = fill("cpu", nv(g)), nodevalue::Vector{Any} = fill(nothing, nv(g)))
-	@assert nv(g) == length(nodelabel) "length of nodelable must be same as number of vertices"
-	@assert nv(g) == length(nodeop) "length of nodeop must be same as number of vertices"
-	@assert nv(g) == length(nodedevice) "length of nodedevice must be same as number of vertices"
-	@assert nv(g) == length(nodevalue) "length of nodevalue must be same as number of vertices"
+	nv(g) == length(nodelabel)  || throw(ArgumentError("length of nodelable must be same as number of vertices"))
+	nv(g) == length(nodeop)  || throw(ArgumentError("length of nodeop must be same as number of vertices"))
+	nv(g) == length(nodedevice)  || throw(ArgumentError("length of nodedevice must be same as number of vertices"))
+	nv(g) == length(nodevalue)  || throw(ArgumentError("length of nodevalue must be same as number of vertices"))
 	summ = SummaryCollection(graph_summary(g, nodelabel, nodeop, nodedevice, nodevalue))
     write_event(logger.file, make_event(logger, summ, step=step))
 end
 
 function graph_summary(g, nodelabel, nodeop, nodedevice, nodevalue)
-	function getdtype(dtype)
-		nodetype =
-		dtype == nothing			? _DataType.DT_INVALID :
-		dtype == UInt8				? _DataType.DT_UINT8 :
-		dtype == UInt16				? _DataType.DT_UINT16 :
-		dtype == UInt32				? _DataType.DT_UINT32 :
-		dtype == UInt64				? _DataType.DT_UINT64 :
-		dtype == Int8				? _DataType.DT_INT8 :
-		dtype == Int16				? _DataType.DT_INT16 :
-		dtype == Int32				? _DataType.DT_INT32 :
-		dtype == Int64				? _DataType.DT_INT64 :
-		dtype == Float16			? _DataType.DT_BFLOAT16 :
-		dtype == Float32			? _DataType.DT_FLOAT :
-		dtype == Float64			? _DataType.DT_DOUBLE :
-		dtype <: AbstractString		? _DataType.DT_STRING :
-		dtype == Bool				? _DataType.DT_BOOL :
-		dtype ∈ [Complex{Float32},
-				Complex{Float16},
-				Complex{UInt8},
-				Complex{UInt16},
-				Complex{UInt32},
-				Complex{Int8},
-				Complex{Int16},
-				Complex{Int32}]		? _DataType.DT_COMPLEX64 :
-		dtype ∈ [Complex{Float64},
-				Complex{UInt64},
-				Complex{Int64}]		? _DataType.DT_COMPLEX128 :
-		@error "Unknown Datatype" dtype
-	end
 	nodes = Vector{NodeDef}()
 	for v in vertices(g)
 		name = nodelabel[v]
@@ -50,19 +21,19 @@ function graph_summary(g, nodelabel, nodeop, nodedevice, nodevalue)
 		x = nodevalue[v]
 		if isa(x, AbstractString)
 			attr["value"] = AttrValue(s = Vector{UInt8}(x))
-			attr["dtype"] = AttrValue(_type = getdtype(typeof(x)))
+			attr["dtype"] = AttrValue(_type = jltype2tf(typeof(x)))
 		elseif isa(x, Integer)
 			attr["value"] = AttrValue(i = Int64(x))
-			attr["dtype"] = AttrValue(_type = getdtype(typeof(x)))
+			attr["dtype"] = AttrValue(_type = jltype2tf(typeof(x)))
 		elseif isa(x, Real)
 			attr["value"] = AttrValue(f = Float32(x))
-			attr["dtype"] = AttrValue(_type = getdtype(typeof(x)))
+			attr["dtype"] = AttrValue(_type = jltype2tf(typeof(x)))
 		elseif isa(x, Bool)
 			attr["value"] = AttrValue(b = x)
-			attr["dtype"] = AttrValue(_type = getdtype(typeof(x)))
+			attr["dtype"] = AttrValue(_type = jltype2tf(typeof(x)))
 		elseif isa(x, AbstractArray)
 			shape = TensorShapeProto(dim = [TensorShapeProto_Dim(size = d) for d in (collect(size(x)))])
-			t = TensorProto(dtype = getdtype(eltype(x)), tensor_shape = shape, tensor_content = serialize_proto(string(x)))
+			t = TensorProto(dtype = jltype2tf(eltype(x)), tensor_shape = shape, tensor_content = serialize_proto(string(x)))
 			attr["value"] = AttrValue(tensor = t)
 			attr["_output_shapes"] = AttrValue(list = AttrValue_ListValue(shape = [shape]))
 		elseif isa(x, Tuple)
@@ -74,10 +45,40 @@ function graph_summary(g, nodelabel, nodeop, nodedevice, nodevalue)
 		elseif x == nothing
 			#donothing
 		else
-			@error "unhandled nodevalue type $(typeof(x))"
+			throw("unhandled nodevalue type $(typeof(x))")
 		end
 		node = NodeDef(name = name, op = op, input = input, device = device, attr = attr)
 		push!(nodes, node)
 	end
 	GraphDef(node = nodes)
+end
+
+function jltype2tf(dtype::DataType)
+	nodetype =
+	dtype == nothing			? _DataType.DT_INVALID :
+	dtype == UInt8				? _DataType.DT_UINT8 :
+	dtype == UInt16				? _DataType.DT_UINT16 :
+	dtype == UInt32				? _DataType.DT_UINT32 :
+	dtype == UInt64				? _DataType.DT_UINT64 :
+	dtype == Int8				? _DataType.DT_INT8 :
+	dtype == Int16				? _DataType.DT_INT16 :
+	dtype == Int32				? _DataType.DT_INT32 :
+	dtype == Int64				? _DataType.DT_INT64 :
+	dtype == Float16			? _DataType.DT_BFLOAT16 :
+	dtype == Float32			? _DataType.DT_FLOAT :
+	dtype == Float64			? _DataType.DT_DOUBLE :
+	dtype <: AbstractString		? _DataType.DT_STRING :
+	dtype == Bool				? _DataType.DT_BOOL :
+	dtype ∈ [Complex{Float32},
+			Complex{Float16},
+			Complex{UInt8},
+			Complex{UInt16},
+			Complex{UInt32},
+			Complex{Int8},
+			Complex{Int16},
+			Complex{Int32}]		? _DataType.DT_COMPLEX64 :
+	dtype ∈ [Complex{Float64},
+			Complex{UInt64},
+			Complex{Int64}]		? _DataType.DT_COMPLEX128 :
+	throw("Unknown Datatype: $dtype")
 end
