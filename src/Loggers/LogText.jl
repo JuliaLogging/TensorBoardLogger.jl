e84fe804ddac02764a62205bdaa8bf4b0cfa2e33
Filippo Vicentini <filippovicentini@gmail.com>
2020-02-13 16:01:19 +0100

    slightly refactor LogText tensor construction so that it can be reused in the future

--- src/Loggers/LogText.jl
+++ src/Loggers/LogText.jl
@@ -9,38 +9,56 @@ function log_text(logger::TBLogger, name::String, text::Any; step = nothing)
     write_event(logger.file, make_event(logger, summ, step=step))
 end
 
-function text_summary(name::String, text::Any)
-    #Create a string tensor
-    #shape of the tensor
-    dims = Array{TensorShapeProto_Dim, 1}()
-    if isa(text, AbstractArray)
-        for dim in size(text)
-            push!(dims, TensorShapeProto_Dim(size = dim))
-        end
-    else
-        push!(dims, TensorShapeProto_Dim(size = 1))
-    end
-    texttensorshape = TensorShapeProto(dim = Vector(dims))
+"""
+    construct_tensor_dims(data) -> TensorShapeProto_Dim
 
-    #content of the tensor
-    textstringval = Vector{Array{UInt8, 1}}()
-    if isa(text, AbstractArray)
-        if ndims(text) == 2
-            text = permutedims(text, (2, 1))
-        end
-        for string in text
-            string = markdown_repr(string)
-            push!(textstringval, Array{UInt8, 1}(string))
-        end
-    else
-        text = markdown_repr(text)
-        push!(textstringval, Array{UInt8, 1}(text))
+Constructs the `TensorShapeProto_Dim` protobuffer holding the size (and eventually
+name) of a single dimension of a tensor/array.
+
+If you define a custom array type, such as AxisArray or named array types,
+you should define this method so that you can serialize the name to string.
+"""
+function construct_tensor_dims(data::AbstractArray)
+    dims = Vector{TensorShapeProto_Dim}()
+    for dim in size(data)
+        push!(dims, TensorShapeProto_Dim(size = dim))
     end
-    #metadata for the text
+    return dims
+end
+
+construct_tensor_dims(data) = [TensorShapeProto_Dim(size=1)]
+
+function construct_tensor_string_val(data::AbstractArray)
+    textstringval = Vector{Vector{UInt8}}()
+
+    if ndims(data) == 2
+        text = permutedims(data, (2, 1))
+    end
+
+    for string in data
+        string = markdown_repr(string)
+        push!(textstringval, Vector{UInt8}(string))
+    end
+
+    return textstringval
+end
+
+construct_tensor_string_val(data) = [Vector{UInt8}(markdown_repr(data))]
+
+function text_summary(name::String, text::Any)
+    # Create a string tensor
+    textstringval = construct_tensor_string_val(text)
+
+    # Structure holding the shape of the tensor
+    dims = construct_tensor_dims(text)
+    texttensorshape = TensorShapeProto(dim = dims)
+
+    # Metadata for the text
     textcontent = serialize_proto(TextPluginData(version = 0))
     plugindata = SummaryMetadata_PluginData(plugin_name = "text", content = textcontent)
     smd = SummaryMetadata(plugin_data = plugindata)
-    #create tensor
+
+    # Create the tensor
     texttensor = TensorProto(dtype = _DataType.DT_STRING, string_val = textstringval, tensor_shape = texttensorshape)
     Summary_Value(tag = name, metadata = smd, tensor = texttensor)
 end
