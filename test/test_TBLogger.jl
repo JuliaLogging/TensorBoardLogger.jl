b95ddb91714277fbdab1b71cc9ff2d832e5277a3
Filippo Vicentini <filippovicentini@gmail.com>
2020-02-14 14:24:41 +0100

    use global folder and cleanup in tests

--- test/test_TBLogger.jl
+++ test/test_TBLogger.jl
@@ -2,45 +2,47 @@ using TensorBoardLogger, Logging
 using TensorBoardLogger: preprocess, summary_impl
 using Test
 
+test_log_dir = "test_logs/"
+
 @testset "Initialization" begin
-    isdir("test_logs/") && rm("test_logs", force=true, recursive=true)
+    isdir(test_log_dir) && rm(test_log_dir, force=true, recursive=true)
 
     # Check tb_append
-    TBLogger("test_logs/run")
-    @test isdir("test_logs/run")
-    TBLogger("test_logs/run") #tb_append by default
-    @test isdir("test_logs/run_1")
-    TBLogger("test_logs/run", tb_increment)
-    @test isdir("test_logs/run_2")
+    TBLogger(test_log_dir*"run")
+    @test isdir(test_log_dir*"run")
+    TBLogger(test_log_dir*"run") #tb_append by default
+    @test isdir(test_log_dir*"run_1")
+    TBLogger(test_log_dir*"run", tb_increment)
+    @test isdir(test_log_dir*"run_2")
 
     # check tb_overwrite
-    close(open("test_logs/run_2/testfile", "w"))
-    TBLogger("test_logs/run_2", tb_overwrite)
-    @test !isfile("test_logs/run_2/testfile")
+    close(open(test_log_dir*"run_2/testfile", "w"))
+    TBLogger(test_log_dir*"run_2", tb_overwrite)
+    @test !isfile(test_log_dir*"run_2/testfile")
 end
 
 @testset "create log directory" begin
     import TensorBoardLogger.init_logdir
-    rm("test_logs/", recursive=true, force=true)
+    rm(test_log_dir, recursive=true, force=true)
 
     # Check tb_append
-    init_logdir("test_logs/run")
-    @test isdir("test_logs/run")
-    init_logdir("test_logs/run") #tb_append by default
-    @test isdir("test_logs/run_1")
-    init_logdir("test_logs/run", tb_increment)
-    @test isdir("test_logs/run_2")
+    init_logdir(test_log_dir*"run")
+    @test isdir(test_log_dir*"run")
+    init_logdir(test_log_dir*"run") #tb_append by default
+    @test isdir(test_log_dir*"run_1")
+    init_logdir(test_log_dir*"run", tb_increment)
+    @test isdir(test_log_dir*"run_2")
 
     # check tb_overwrite
-    close(open("test_logs/run_2/testfile", "w"))
-    init_logdir("test_logs/run_2", tb_overwrite)
-    @test !isfile("test_logs/run_2/testfile")
+    close(open(test_log_dir*"run_2/testfile", "w"))
+    init_logdir(test_log_dir*"run_2", tb_overwrite)
+    @test !isfile(test_log_dir*"run_2/testfile")
 end
 
 @testset "create event file" begin
     import TensorBoardLogger.init_logdir, TensorBoardLogger.create_eventfile
-    rm("test_logs/", recursive=true, force=true)
-    logdir = init_logdir("test_logs/run")
+    rm(test_log_dir, recursive=true, force=true)
+    logdir = init_logdir(test_log_dir*"run")
 
     fname, file = create_eventfile(logdir)
     @test isfile(joinpath(logdir, fname))
@@ -53,11 +55,11 @@ end
     # workaround step being already exported in julia 1.0
     #import TensorBoardLogger.step
     tb_step = TensorBoardLogger.step
-    
-    tbl = TBLogger("test_logs/run", tb_overwrite)
+
+    tbl = TBLogger(test_log_dir*"run", tb_overwrite)
     @test tb_step(tbl) == 0
 
-    tbl = TBLogger("test_logs/run", purge_step=12)
+    tbl = TBLogger(test_log_dir*"run", purge_step=12)
     @test tb_step(tbl) == 12
 
     @test increment_step!(tbl, 1) == 13
@@ -68,7 +70,7 @@ end
 end
 
 @testset "resetting" begin
-    tbl = TBLogger("test_logs/run", tb_overwrite)
+    tbl = TBLogger(test_log_dir*"run", tb_overwrite)
     TensorBoardLogger.add_eventfile(tbl, "pp")
     set_step!(tbl, 100)
 
