764e89f43205f04627484ef2a1d8e47296bac6dd
St Z <abczst@outlook.com>
2021-02-24 16:01:25 +0800

    :bug: fix `TBHistogram` `name` is now correctly sent

--- src/logger_dispatch_overrides.jl
+++ src/logger_dispatch_overrides.jl
@@ -193,7 +193,7 @@ end
 content(x::TBHistogram) = x.data
 preprocess(name, val::TBHistogram{T,N}, data) where {T<:Complex,N} =
     push!(data, name*"/re"=>TBHistogram(real.(content(val))), name*"/im"=>TBHistogram(imag.(content(val))))
-preprocess(name, val::TBHistogram, data) = preprocess(data, fit(Histogram, val.data), data)
+preprocess(name, val::TBHistogram, data) = preprocess(name, fit(Histogram, val.data), data)
 
 """
     TBVector(data)
