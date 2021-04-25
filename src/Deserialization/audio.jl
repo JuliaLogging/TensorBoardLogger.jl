f509d5afe2676da2549694b9a4204f935e6e5667
Filippo Vicentini <filippovicentini@gmail.com>
2021-04-25 13:28:26 +0200

    Fix deprecation warnings

--- src/Deserialization/audio.jl
+++ src/Deserialization/audio.jl
@@ -2,7 +2,7 @@ function deserialize_audio_summary(summary)
     audio = summary.audio
 
     if audio.content_type == "audio/wav"
-        value = load(Stream(format"WAV", PipeBuffer(audio.encoded_audio_string)))
+        value = load(Stream{format"WAV"}(PipeBuffer(audio.encoded_audio_string)))
     else
         value = nothing
         @warn "unknown audio format $(audio.content_type)"
