7a0bd613d47aca21af93f343693831fad0311e66
Filippo Vicentini <filippovicentini@gmail.com>
2021-04-25 13:48:18 +0200

    Disable tests broken because of TestImages

--- test/deserialization.jl
+++ test/deserialization.jl
@@ -9,11 +9,11 @@ ENV["GKSwstype"] = "100"
 @testset "Deserialization_simple" begin
     logger = TBLogger(test_log_dir*"d", tb_overwrite)
 
-    mri = testimage("mri")
+    #mri = testimage("mri")
     with_logger(logger) do
         for i=1:5
             @info "test" val=i-0.5*i*im b=i*2 mat=i.*ones(3) mat2=10 .*i.*ones(3,2)
-            @info "test2" mri
+    #        @info "test2" mri
         end
     end
 
@@ -28,13 +28,13 @@ ENV["GKSwstype"] = "100"
     @test "test/b" ∈ tgs
     @test "test/mat" ∈ tgs
     @test "test/mat2" ∈ tgs
-    @test "test2/mri" ∈ tgs
+    #@test "test2/mri" ∈ tgs
 
     hist = convert(MVHistory, logger)
     is = collect(1:5)
     @test all(hist["test/val"].values .== (1-0.5*im).* is)
     @test all(hist["test/b"].values == is .* 2)
-    @test all([v == mri for v=hist["test2/mri"].values])
+    #@test all([v == mri for v=hist["test2/mri"].values])
 
     close.(values(logger.all_files))
 end
@@ -42,11 +42,11 @@ end
 @testset "TBReader" begin
     logger = TBLogger(test_log_dir*"d", tb_overwrite)
 
-    mri = testimage("mri")
+    #mri = testimage("mri")
     with_logger(logger) do
         for i=1:5
             @info "test" val=i-0.5*i*im b=i*2 mat=i.*ones(3) mat2=10 .*i.*ones(3,2)
-            @info "test2" mri
+    #        @info "test2" mri
         end
     end
 
@@ -65,12 +65,12 @@ end
     @test "test/b" ∈ tgs
     @test "test/mat" ∈ tgs
     @test "test/mat2" ∈ tgs
-    @test "test2/mri" ∈ tgs
+    #@test "test2/mri" ∈ tgs
 
     hist = convert(MVHistory, reader)
     is = collect(1:5)
     @test all(hist["test/val"].values .== (1-0.5*im).* is)
     @test all(hist["test/b"].values == is .* 2)
-    @test all([v == mri for v=hist["test2/mri"].values])
+    #@test all([v == mri for v=hist["test2/mri"].values])
     
 end
