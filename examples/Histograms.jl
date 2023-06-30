1d933fd73362c0d99fa64f7e6652fea6a2e80540
Lior Blech <nomadbl@gmail.com>
2023-06-30 11:20:00 +0300

    fix logger interface for histograms and simplify the code

--- examples/Histograms.jl
+++ examples/Histograms.jl
@@ -10,9 +10,10 @@ with_logger(logger) do
         x0 = 0.5+i/30; s0 = 0.5/(i/20);
         edges = collect(-5:0.1:5)
         centers = collect(edges[1:end-1] .+0.05)
-        histvals = [exp(-((c-x0)/s0)^2) for c = centers]
+        histvals = s0 * randn(length(centers)) .+ x0
         data_tuple = (edges, histvals)
-        @info "histogram/loggerinterface" autobin=rand(10).+0.1*i manualbin=data_tuple
+        @info "histogram/loggerinterface" autobin=s0 .* randn(100) .+ x0
+        @info "histogram/loggerinterface" manualbin=data_tuple
     end
 end
 
@@ -21,8 +22,8 @@ for i in 1:100
     x0 = 0.5+i/30; s0 = 0.5/(i/20);
     edges = collect(-5:0.1:5)
     centers = collect(edges[1:end-1] .+0.05)
-    histvals = [exp(-((c-x0)/s0)^2) for c = centers]
+    histvals = s0 * randn(length(centers)) .+ x0
     data_tuple = (edges, histvals)
-    log_histogram(logger, "histogram/explicitinterface/autobin", rand(10).+0.1*i, step = i) #automatic bins
+    log_histogram(logger, "histogram/explicitinterface/autobin", s0 .* randn(100) .+ x0, step = i) #automatic bins
     log_histogram(logger, "histogram/explicitinterface/manualbin", data_tuple, step = i) #manual bins
 end
