2d554c4333c80fbb451d90d066999392c7e6fa30
Lior Blech <nomadbl@gmail.com>
2023-07-05 17:45:09 +0300

    add helper function, docs and example code

--- examples/Scalars.jl
+++ examples/Scalars.jl
@@ -23,3 +23,28 @@ with_logger(logger) do
         @info "scalar/complex" y = z
     end
 end
+
+
+################control step increments with context################
+with_logger(logger) do
+    for epoch in 1:10
+        for i=1:100
+            # increments global_step by default
+            with_TBLogger_hold_step() do
+                # all of these are logged at the same global_step
+                # and the logger global_step is only then increased
+                @info "train1/scalar" val=i
+                @info "train2/scalar" val2=i/2
+                @info "train3/scalar" val3=100-i
+            end
+        end
+        # step increment at end can be disabled for easy train/test sync
+        with_TBLogger_hold_step(;step_at_end=false) do
+            # all of these are logged at the same global_step
+            # and the logger global_step is only then increased
+            @info "test1/scalar" epoch=epoch
+            @info "test2/scalar" epoch2=epoch^2
+            @info "test3/scalar" epoch3=epoch^3
+        end
+    end
+end
