49e179777b73e806f6244dfe2b382530276ab0e7
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-12 11:29:06 +0000

    First draft attempt at adding hyperparameter support
    Signed-off-by: Jamie Mair <JamieMair@users.noreply.github.com>

--- src/hparams.jl
+++ src/hparams.jl
@@ -1,4 +1,4 @@
-import .tensorboard_plugin_hparams.hparams: Interval, MetricInfo, MetricName, HParamInfo
+import .tensorboard_plugin_hparams.hparams: Interval, MetricInfo, MetricName, HParamInfo, Experiment, HParams
 import .tensorboard_plugin_hparams.hparams: var"#DataType" as HParamDataType
 import .tensorboard_plugin_hparams.google.protobuf: ListValue as HListValue, OneOf as HOneOf, Value as HValue
 struct HParamRealDomain
@@ -7,6 +7,19 @@ struct HParamRealDomain
 end
 struct HParamSetDomain{T<:Union{String, Bool, Float64}}
     values::Vector{T}
+end
+Base.@kwdef struct HParamConfig
+    name::String
+    datatype::DataType
+    displayname::String = ""
+    description::String = ""
+    domain::Union{Nothing, HParamRealDomain, HParamSetDomain}
+end
+Base.@kwdef struct MetricConfig
+    name::String
+    datatype::DataType
+    displayname::String = ""
+    description::String = ""
 end
 
 default_domain(::Val{Bool}) = HParamSetDomain([true, false])
@@ -32,9 +45,10 @@ _convert_hparam_domain(domain::HParamRealDomain) = HOneOf(:domain_interval, Inte
 _convert_hparam_domain(domain::HParamSetDomain) = HOneOf(:domain_discrete, HListValue([_convert_value(v) for v in domain.values]))
 
 
-function hparam_info(name, datatype; displayname="", description="", domain=nothing)
-    if isnothing(domain)
+function hparam_info(c::HParamConfig)
+    domain = if isnothing(c.domain)
         domain = default_domain(Val(datatype))
+        domain
     else
         if isa(domain, HParamRealDomain)
             @assert datatype==Float64 "Real domains require Float64"
@@ -45,9 +59,56 @@ function hparam_info(name, datatype; displayname="", description="", domain=noth
         elseif isa(domain, HParamSetDomain{Float64})
             @assert datatype==Float64 "Domains with floats require a datatype of Float64"
         end
+
+        c.domain
     end
     
-    dtype = _to_proto_hparam_dtype(Val(datatype))
+    dtype = _to_proto_hparam_dtype(Val(c.datatype))
     converted_domain = _convert_hparam_domain(domain)
-    return HParamInfo(name, displayname, description, dtype, converted_domain)
+    return HParamInfo(c.name, c.displayname, c.description, dtype, converted_domain)
 end
+function decode_metric_name(name)
+    if contains(name, '/')
+        return (split(name, '/', limit=2)...)
+    else
+        return ("", name)
+    end
+end
+function metric_info(c::MetricConfig)
+    dtype = _to_proto_hparam_dtype(Val(c.datatype))
+    
+    group, tag = decode_metric_name(c.name)
+    mname = MetricName(group, tag)
+    return MetricInfo(mname, c.displayname, c.description, dtype)
+end
+
+function init_hparams!(logger::TBLogger, param_configs::AbstractArray{HParamConfig}, metric_configs::AbstractArray{MetricConfig}; name="", description="", user="")
+     # Convert to proto types
+    hparam_infos = hparam_info.(param_configs)
+    metric_infos = metric_configs.(metric_configs)
+    # Create the proto experiment type to serialise
+    experiment = Experiment(name, description, user, zero(Float64), hparam_infos, metric_infos)
+
+    # Write this event to a separate file
+    filename = add_eventfile(logger, "hparams_config")
+    eventfile = logger.all_files[filename]
+    write_event(eventfile, experiment)
+    nothing
+end
+
+function write_hparams!(logger::TBLogger, hparams::Dict{String, Any})
+    # Check the input types
+    for v in values(hparams)
+        @assert typeof(v) <: Union{Bool, String, Float64} "Hyperparameters must be of types String, Bool or Float64"
+    end
+    # Convert to protobuf types
+    hparams = HParams(Dict{String, HValue}(k=>_convert_value(v) for (k,v) in hparams))
+
+    # Write the configuration to a new event file
+    filename = add_eventfile(logger, "hparams")
+    eventfile = logger.all_files[filename]
+    write_event(eventfile, hparams)
+    nothing
+end
+
+export init_hparams!, write_hparams!, HParamRealDomain, HParamSetDomain, HParamConfig, MetricConfig
