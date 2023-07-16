867b10534c5bd0de52e8248e7d8b945db6c2c73d
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-16 11:24:35 +0100

    Tidy up and use normal PB OneOf, switch to allowing Real instead of just Float64

--- src/hparams.jl
+++ src/hparams.jl
@@ -1,6 +1,6 @@
 import .tensorboard_plugin_hparams.hparams: Interval, MetricInfo, MetricName, HParamInfo, Experiment, HParams
 import .tensorboard_plugin_hparams.hparams: var"#DataType" as HParamDataType, DatasetType as HDatasetType
-import .tensorboard_plugin_hparams.google.protobuf: ListValue as HListValue, OneOf as HOneOf, Value as HValue
+import .tensorboard_plugin_hparams.google.protobuf: ListValue as HListValue, Value as HValue
 import .tensorboard_plugin_hparams.hparams as HP
 import .tensorboard: DataClass
 
@@ -29,24 +29,24 @@ default_domain(::Val{Float64}) = HParamRealDomain(typemin(Float64), typemax(Floa
 default_domain(::Val{String}) = nothing
 
 _to_proto_hparam_dtype(::Val{Bool}) = HParamDataType.DATA_TYPE_BOOL
-_to_proto_hparam_dtype(::Val{String}) = HParamDataType.DATA_TYPE_STRING
 _to_proto_hparam_dtype(::Val{Float64}) = HParamDataType.DATA_TYPE_FLOAT64
+_to_proto_hparam_dtype(::Val{String}) = HParamDataType.DATA_TYPE_STRING
 
-function _convert_value(v::T) where {T<:Union{String, Bool, Float64}}
+function _convert_value(v::T) where {T<:Union{String, Bool, Real}}
     if v isa String
-        return HValue(HOneOf(:string_value, v))
+        return HValue(OneOf(:string_value, v))
     elseif v isa Bool
-        return HValue(HOneOf(:bool_value, v))
-    elseif v isa Float64
-        return HValue(HOneOf(:number_value, v))
+        return HValue(OneOf(:bool_value, v))
+    elseif v isa Real
+        return HValue(OneOf(:number_value, Float64(v)))
     else
         error("Unrecognised type!")
     end
 end
 
 _convert_hparam_domain(::Nothing) = nothing
-_convert_hparam_domain(domain::HParamRealDomain) = HOneOf(:domain_interval, Interval(domain.min_value, domain.max_value))
-_convert_hparam_domain(domain::HParamSetDomain) = HOneOf(:domain_discrete, HListValue([_convert_value(v) for v in domain.values]))
+_convert_hparam_domain(domain::HParamRealDomain) = OneOf(:domain_interval, Interval(domain.min_value, domain.max_value))
+_convert_hparam_domain(domain::HParamSetDomain) = OneOf(:domain_discrete, HListValue([_convert_value(v) for v in domain.values]))
 
 
 function hparam_info(c::HParamConfig)
@@ -62,7 +62,7 @@ function hparam_info(c::HParamConfig)
         elseif isa(domain, HParamSetDomain{Bool})
             @assert datatype==Bool "Domains with bools require a datatype of Bool"
         elseif isa(domain, HParamSetDomain{Float64})
-            @assert datatype==Float64 "Domains with floats require a datatype of Float64"
+            @assert datatype<:Real "Domains with floats require a datatype a Real datatype"
         end
     end
     
@@ -91,7 +91,7 @@ function write_hparams!(logger::TBLogger, hparams::Dict{String, Any}, metrics::A
 
     # Check for datatypes
     for v in values(hparams)
-        @assert typeof(v) <: Union{Bool, String, Float64} "Hyperparameters must be of types String, Bool or Float64"
+        @assert typeof(v) <: Union{Bool, String, Real} "Hyperparameters must be of types String, Bool or Real"
     end
 
     hparam_infos = [hparam_info(HParamConfig(; name=k, datatype=typeof(v))) for (k, v) in hparams]
@@ -101,18 +101,18 @@ function write_hparams!(logger::TBLogger, hparams::Dict{String, Any}, metrics::A
     hparams_dict = Dict{String, Any}(k=>_convert_value(v) for (k,v) in hparams)
 
     session_start_info = HP.SessionStartInfo(hparams_dict, "", "", "", zero(Float64))
-    session_start_content = HP.HParamsPluginData(PLUGIN_DATA_VERSION, HOneOf(:session_start_info, session_start_info))
+    session_start_content = HP.HParamsPluginData(PLUGIN_DATA_VERSION, OneOf(:session_start_info, session_start_info))
     session_start_md = SummaryMetadata(SummaryMetadata_PluginData(PLUGIN_NAME, encode_bytes(session_start_content)), PLUGIN_NAME, "", DataClass.DATA_CLASS_UNKNOWN)
     session_start_summary = Summary([Summary_Value("", SESSION_START_INFO_TAG, session_start_md, nothing)])
     
     experiment = HP.Experiment("", "", "", zero(Float64), hparam_infos, metric_infos)
-    experiment_content = HP.HParamsPluginData(PLUGIN_DATA_VERSION, HOneOf(:experiment, experiment))
-    experiment_md = SummaryMetadata(SummaryMetadata_PluginData(PLUGIN_NAME, encode_bytes(experiment_content)), PLUGIN_NAME, "", DataClass.DATA_CLASS_UNKNOWN)
+    experiment_content = HP.HParamsPluginData(PLUGIN_DATA_VERSION, OneOf(:experiment, experiment))
+    experiment_md = SummaryMetadata(SummaryMetadata_PluginData(PLUGIN_NAME, encode_bytes(experiment_content)), "", "", DataClass.DATA_CLASS_UNKNOWN)
     experiment_summary = Summary([Summary_Value("", EXPERIMENT_TAG, experiment_md, nothing)])
 
     session_end_info = HP.SessionEndInfo(HP.Status.STATUS_SUCCESS, zero(Float64))
-    session_end_content = HP.HParamsPluginData(PLUGIN_DATA_VERSION, HOneOf(:session_end_info, session_end_info))
-    session_end_md = SummaryMetadata(SummaryMetadata_PluginData(PLUGIN_NAME, encode_bytes(session_end_content)), PLUGIN_NAME, "", DataClass.DATA_CLASS_UNKNOWN)
+    session_end_content = HP.HParamsPluginData(PLUGIN_DATA_VERSION, OneOf(:session_end_info, session_end_info))
+    session_end_md = SummaryMetadata(SummaryMetadata_PluginData(PLUGIN_NAME, encode_bytes(session_end_content)), "", "", DataClass.DATA_CLASS_UNKNOWN)
     session_end_summary = Summary([Summary_Value("", SESSION_END_INFO_TAG, session_end_md, nothing)])
 
     for s in (experiment_summary, session_start_summary, session_end_summary)
